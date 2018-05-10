f1dac6e4a8011c554cd9a8f4e8be5abe7c715284
Preet s <preet@ool-ad020795.dyn.optonline.net>
2018-05-10 12:32:59 -0400

    added card click and timer function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $name = $('.name');
 
 let name = prompt("Please enter your name");
+let timer = $('.timer');
+let showCard = $('.show-card');
+let card = $('.card');
+let count = 5;
+
+
 
 if (name) {
   $('.name').html(name);
@@ -17,7 +23,7 @@ let newDeck = [];
 function getDeck() {
 
   for (let cardValue of values) {
-  for (let cardSuit of suits) {
+    for (let cardSuit of suits) {
 
       deck.push({
         suit: cardSuit,
@@ -29,8 +35,8 @@ function getDeck() {
 }
 
 function addRank() {
-  for(let i = 0; i< deck.length; i++) {
-    if (deck[i].value >=2 && deck[i].value <= 6) {
+  for (let i = 0; i < deck.length; i++) {
+    if (deck[i].value >= 2 && deck[i].value <= 6) {
       deck[i].rank = 1;
     } else if (deck[i].value >= 6 && deck[i].value <= 9) {
       deck[i].rank = 0;
@@ -44,23 +50,48 @@ function addRank() {
 
 
 
-function buildDeck () {
+function buildDeck() {
   getDeck();
   addRank();
 }
-
+//build deck with value and rank
 buildDeck();
-console.table(deck);
 
 
 
+
+//shuffle deck
+function shuffleDeck() {
+  for (i = deck.length; i > 0; i--) {
+    let getRandCard = Math.floor(Math.random() * 52);
+    // let getRandCard = Math.floor(getNum);
+    newDeck.push(deck[getRandCard]);
+  }
+}
+shuffleDeck();
+// console.table(newDeck)
+// console.table(newDeck).pop;
+
+// show card on page
+card.on("click", function(e){
+  let x = newDeck.pop();
+  card.append(x.value, x.suit);
+});
+
+
+
+
+//set timer for player.
+// let oneSec = setInterval(time, 1000);
 //
-// function shuffleDeck() {
-//   for (i = deck.length; i > 0; i--) {
-//     let getNum = Math.random() * 52;
-//     let getRandCard = Math.floor(getNum);
-//     newDeck.push(deck[getRandCard]);
+// function time() {
+//   timer.html(count);
+//   count--;
+//   if (count === -1){
+//     alert("Time is up!!");
+//     clearInterval(oneSec);
 //   }
-//
-//   console.log(newDeck);
 // }
+
+
+// time();
